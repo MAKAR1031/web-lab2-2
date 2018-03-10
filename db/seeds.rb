9d9ef7917e48f6d0517497a9ddaa7edc1b83f9a5
Makarov Sergey <makar1031@yandex.ru>
2018-03-10 23:08:51 +0300

    заполнение  таблицы products данными

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([
+    { name: 'Excalibur', price: 520.30 },
+    { name: 'Shining Armor', price: 870.05 },
+    { name: 'Helmet of the Arena Champion', price: 290.70 },
+    { name: 'Fuzzies Potion', price: 30 },
+    { name: 'Healing Potion', price: 74.2 },
+    { name: 'Potion of Pure Reason', price: 89.7 },
+    { name: 'Scroll of Resurrection', price: 132.5 }
+])
